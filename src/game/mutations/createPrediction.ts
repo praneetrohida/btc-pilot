b2f90186de9ae972160ea737c77332abebf5f307
Praneet Rohida <praneetrohida@gmail.com>
2023-08-04 19:54:43 +0530

    add timeout to score the user after 60 seconds

--- src/game/mutations/createPrediction.ts
+++ src/game/mutations/createPrediction.ts
@@ -1,6 +1,8 @@
 import { resolver } from "@blitzjs/rpc";
 import { z } from "zod";
 import db, { PredictionDirection } from "db";
+import ms from "ms";
+import { getLatestBtcPrice } from "src/game/utils/getLatestBtcPrice";
 
 const CreatePredictionSchema = z.object({
   price: z.number(),
@@ -12,7 +14,7 @@ export default resolver.pipe(
   async ({ price, direction }, ctx) => {
     if (!ctx.session.userId) throw new Error("Unauthorized");
     // This throws an error if credentials are invalid
-    return db.prediction.create({
+    const prediction = await db.prediction.create({
       data: {
         price,
         direction,
@@ -23,5 +25,36 @@ export default resolver.pipe(
         },
       },
     });
+
+    setTimeout(async () => {
+      const newPrice = await getLatestBtcPrice();
+
+      await db.prediction.update({
+        where: {
+          id: prediction.id,
+        },
+        data: {
+          laterPrice: newPrice,
+        },
+      });
+
+      if (newPrice === price) return;
+      // price stayed the same, no points
+
+      const isPredictionCorrect =
+        (prediction.direction === PredictionDirection.UP && newPrice > prediction.price) ||
+        (prediction.direction === PredictionDirection.DOWN && newPrice < prediction.price);
+
+      await db.user.update({
+        where: {
+          id: ctx.session.userId,
+        },
+        data: {
+          score: {
+            increment: isPredictionCorrect ? 1 : -1,
+          },
+        },
+      });
+    }, ms("60s"));
   }
 );
